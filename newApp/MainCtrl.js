967e223d373b182af915231d327b0bb029add33f
shmuel <disraelishmuel@gmail.com>
2016-04-06 21:41:43 +0300

    finished the search bar functionalty + seaerch results view including back btn

--- newApp/MainCtrl.js
+++ newApp/MainCtrl.js
@@ -12,10 +12,9 @@
         console.log(this.currentItem);
     };
 
-    MainCtrl.prototype.displayCurrentItem = function(id){
-    //    get item by "id"
-        var item = this.phoneBook.getItemById(id);
-        //    chek if it is root group or contact
+    MainCtrl.prototype.displayCurrentItem = function(item){
+
+        //    chek if it is root group or contact or search results item
         //    & update the url value to the proper partial template url accordingly
         if (item.fName){
             this.currentItem = item;
@@ -29,12 +28,20 @@
             this.currentItem = item;
             this.currentItem.url = "newApp/partials/root.html";
         }
+        else if (item.searchResult){
+            this.currentItem = item;
+            this.currentItem.url = "newApp/partials/searchResults.html";
+        }
         this.hideForm();
 
     };
 
     MainCtrl.prototype.up = function(){
-        this.displayCurrentItem(this.currentItem.parent.id);
+        this.displayCurrentItem(this.currentItem.parent);
+    };
+
+    MainCtrl.prototype.back = function(){
+        this.displayCurrentItem(this.currentItem.backItem);
     };
 
     MainCtrl.prototype.hideForm = function(){
@@ -98,8 +105,9 @@
             Materialize.toast(content, 4000);
         })
     };
-
+    //when the user blurs the edit mode or press enter
     MainCtrl.prototype.updateItemName = function(event){
+        //the freshly edited item name
         var newName = event.target.textContent.trim();
 
         if (newName != ""){
@@ -110,14 +118,36 @@
             }
             else if (event.type == "keypress" && event.keyCode == 13){
                 event.preventDefault();
-                event.target.blur();
+                event.target.blur();//which fires the blur event and does all the above
             }
         }
         else {
+            //if the user submited a blank string then its probbely a mistake or that he wishes to delete the item
             //    todo ask the user if he wishs to delete he item if not do nothing
             event.preventDefault();
         }
 
+    };
+
+    MainCtrl.prototype.deleteItem = function(){
+        this.phoneBook.deleteItem(this.currentItem.id);
+        var content = '<i class="material-icons small red-text">delete</i><span>' + this.currentItem.name + ' was deleted </span>';
+        Materialize.toast(content, 4000);
+        this.currentItem = this.currentItem.parent;
+    };
+
+    MainCtrl.prototype.search = function(event) {
+        var results = this.phoneBook.search(this.searchParam);
+        var resObj = {
+            searchResult:true,
+            searchParam:this.searchParam,
+            childItems:results,
+            backItem:this.currentItem
+        };
+        this.displayCurrentItem(resObj);
+        this.searchParam = "";
+        var inputField = event.target.firstElementChild.firstElementChild;
+        inputField.blur();
     };
 
     app.controller('MainCtrl',['phoneBookService',MainCtrl]);
